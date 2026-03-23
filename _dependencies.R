bf5a226059af4b78bc3b340af12f249fd606bc75
Alex Chubaty <achubaty@for-cast.ca>
2026-03-23 14:26:35 -0600

    add _dependencies.R to appease renv

---
+++ _dependencies.R
@@ -0,0 +1,11 @@
+## indirect package dependencies to appease renv
+if (FALSE) {
+  library(archive)
+  library(cluster)
+  library(foreign)
+  library(golem) ## used by nrv.forsite.app
+  library(httpuv) ## used by googledrive
+  library(rpart)
+  library(shiny) ## used by nrv.forsite.apps
+  library(spatial)
+}
